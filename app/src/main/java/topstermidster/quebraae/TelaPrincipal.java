56059b9d3e36335bb3a97fef6f647c416561c3c5
Anderson Ancilon <anderson.santos@tre-to.jus.br>
2018-08-22 17:50:57 -0300

    Método inicial de criptografia inserido, tela inicial de criptografia inserido.

--- app/src/main/java/topstermidster/quebraae/TelaPrincipal.java
+++ app/src/main/java/topstermidster/quebraae/TelaPrincipal.java
@@ -2,15 +2,14 @@ package topstermidster.quebraae;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.InputFilter;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class TelaPrincipal extends AppCompatActivity {
 
@@ -20,22 +19,63 @@ public class TelaPrincipal extends AppCompatActivity {
     private String cifrado = "";
     private String chave;
 
+    final int qtd = 127;
+
+
     public void criptografar(String textoClaro) {
 
-        for(int i = 0; i < textoClaro.length(); i++) {
+       /* for(int i = 0; i < textoClaro.length(); i++) {
             char caractere = textoClaro.charAt(i);
             int decimal = (int) caractere;
 
-            Log.v("valor", " decimal" + decimal + " Na posicao: " + i );
+            //Log.v("valor", " decimal" + decimal + " Na posicao: " + i );
             chave += (int) caractere + ";";
 
             if(decimal != 32)
                 decimal = decimal + 3;
             cifrado += (char) decimal;
+        }*/
+        contagemCaracteres(textoClaro);
+    }
+
+    public String contagemCaracteres(String textoClaro) {
+        Map<Character, Integer> mapaLetras = new HashMap<Character, Integer>();
+
+        char arr[] = textoClaro.toCharArray();
+
+        for( char letra: arr) {
+            if (!mapaLetras.containsValue(letra)) {
+                mapaLetras.put(letra, 0);
+            }
+
+            mapaLetras.put(letra, mapaLetras.get(letra) + 1);
+        }
+
+        Iterator<Integer> itr = mapaLetras.values().iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
         }
 
 
+        return "";
     }
+
+
+
+
+    public void fatorial(int qtd) {
+        int fat = 1;
+
+        for( int i = 2; i <= qtd; i++ )
+        {
+            fat *= i;
+        }
+    }
+
+
+
+
+
 
 
     @Override
