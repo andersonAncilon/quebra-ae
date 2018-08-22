0815cb39724b76cc42e9d9d1eeb482a3f7678532
Anderson Ancilon <anderson.santos@tre-to.jus.br>
2018-08-22 13:22:05 -0300

    Commit inicial

---
+++ app/src/main/java/topstermidster/quebraae/TelaPrincipal.java
@@ -0,0 +1,13 @@
+package topstermidster.quebraae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal);
+    }
+}
